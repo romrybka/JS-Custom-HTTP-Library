c9b0149ee40c9b34efc1694ab23650fa0e918b88
romrybka <rprybka@gmail.com>
2018-06-21 11:23:16 +0300

    easyHTTP Library is done (AJAX, callbacks) GET, POST, PUT, DELETE

---
+++ app.js
@@ -0,0 +1,52 @@
+const http = new easyHTTP();
+
+// Get posts
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+//   if (!err) {
+//     console.log(posts);
+//   } else {
+//     console.log(err);
+//   }
+// });
+
+// Get Single post
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, post) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+ 
+// Create Data
+const data = {
+  title: 'Custom Post',
+  body: 'This is a custom post'
+};
+
+// Create post
+http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post) {
+  if (err) {
+      console.log(err);
+    } else {
+      console.log(post);
+    }
+})
+
+// Update post
+// http.put('https://jsonplaceholder.typicode.com/posts/55', data, function(err, post) {
+//   if (err) {
+//       console.log(err);
+//     } else {
+//       console.log(post);
+//     }
+// })
+
+// Delete post
+// http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response) {
+//   if (!err) {
+//     console.log(response);
+//   } else {
+//     console.log(err);
+//   }
+// });
